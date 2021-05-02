78172a6d93525129040e227491a7b4ce88a1c7ba
codessstein <dmytromorozzz@gmail.com>
2021-05-02 22:55:09 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AsteroidsComponent } from './pages/asteroids/asteroids.component';
+import { DayspictureComponent } from './pages/dayspicture/dayspicture.component';
+import { SubmitPlanetComponent } from './pages/submit-planet/submit-planet.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainMenuComponent } from './shared/navigation/main-menu/main-menu.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    HomeComponent,
+    AsteroidsComponent,
+    DayspictureComponent,
+    SubmitPlanetComponent,
+    NotFoundComponent,
+    MainMenuComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/dayspicture/dayspicture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dayspicture',
+  templateUrl: './dayspicture.component.html',
+  styleUrls: ['./dayspicture.component.scss']
+})
+export class DayspictureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
