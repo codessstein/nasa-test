2b4e084d4d2a5c009870b8b400811f9c4ca7fc53
codessstein <dmytromorozzz@gmail.com>
2021-05-03 13:35:23 +0300

    added en, uk, ru languages

--- src/app/shared/navigation/lang-switch/lang-switch.component.ts
+++ src/app/shared/navigation/lang-switch/lang-switch.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+
 
 @Component({
   selector: 'app-lang-switch',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lang-switch.component.scss']
 })
 export class LangSwitchComponent implements OnInit {
+  public languages = [
+    {
+      title: 'EN',
+      code: 'en',
+    },
+    {
+      title: 'УКР',
+      code: 'uk'
+    },
+    {
+      title: 'РУС',
+      code: 'ru'
+    }
+  ] 
+  constructor(public translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
 
-  constructor() { }
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('en');
+}
 
   ngOnInit(): void {
   }
-
 }
